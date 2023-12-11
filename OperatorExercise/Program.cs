eed16e44f949521bd4c987094398569078bb7e66
Mariam Khamisi <mariam5100@yahoo.com>
2023-12-11 12:16:13 +0300

    fourth commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace OperatorExercise
 {
     public class Program
+        
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int a = 17;
+            int b = 4;
+            var sum = a + b;
+            var subtraction = a - b;
+            var multiplication = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            Console.WriteLine($"Addition: {sum}");
+            Console.WriteLine($"Subtraction: {subtraction}");
+            Console.WriteLine($"Multiplication: {multiplication}");
+            Console.WriteLine($"Division: {quotient}");
+            Console.WriteLine($"Modulus: {remainder}");
+
+
         }
     }
 }
